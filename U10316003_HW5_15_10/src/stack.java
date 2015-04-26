b33747a3c06c5bfa9d063d5d8de8b36ad194ff13
er201024 <er201024@gmail.com>
2015-04-26 09:38:02 +0800

    15.10

--- U10316003_HW5_15_10/src/stack.java
+++ U10316003_HW5_15_10/src/stack.java
@@ -1,60 +1,61 @@
 import java.lang.Cloneable;
 import java.util.ArrayList;
-
 import static java.lang.System.out;
 
 class MyStack implements  Cloneable{
-	
+	//new ArrayList name List
 	public ArrayList<Object> list = new ArrayList<Object>();
 	
-	
-  public boolean isEmpty() {
-    return isEmpty();
-  }
-
-  public int getSize() {
-    return getSize();
-  }
-
-  public Object peek() {
-    return list.get(getSize() - 1);
-  }
-
-  public Object pop() {
-    Object o = list.get(getSize() - 1);
-    list.remove(getSize() - 1);
-    return o;
-  }
-
-  public void push(Object o) {
-    list.add(o);
-  }
-  @Override
-  public Object clone() throws CloneNotSupportedException{
-	
-		
-	  MyStack clone = (MyStack)super.clone();
-	  clone.list = (ArrayList<Object>)(list.clone());
-
-	  return clone;
-	} 
- /** Override the toString in the Object class */
- @Override 
- public String toString() {
-    return "stack: " + toString();
-  }
+	//Check stack empty
+	public boolean isEmpty() {
+		return isEmpty();
+	}
+	//Get size
+	public int getSize() {
+		return getSize();
+	}
+	//Peek stack
+	public Object peek() {
+		return list.get(getSize() - 1);
+	}
+	//remove string
+	public Object pop() {
+		Object o = list.get(getSize() - 1);
+		list.remove(getSize() - 1);
+		return o;
+	}
+	//push stack
+	public void push(Object o) {
+		list.add(o);
+	}
+	@Override
+	public Object clone() throws CloneNotSupportedException{
+		//super clone
+		MyStack clone = (MyStack)super.clone();
+		//deep copy
+		clone.list = (ArrayList<Object>)(list.clone());
+		return clone;
+		} 
+	/** Override the toString in the Object class */
+	@Override 
+	public String toString() {
+		return "stack: " + toString();
+	}
  
 }
 
-public class stack{
- public static void main(String[] args) throws CloneNotSupportedException{
-		
+	public class stack{
+		/*main method*/
+		public static void main(String[] args) throws CloneNotSupportedException{	
+		//call list1
 		MyStack list1 = new MyStack();
+		//call list2
 		MyStack list2 = (MyStack)(list1.clone());
+		//print list1 if not equal list2 is false
 		out.print("list1 == list2 ? "+ (list1 == list2) + "\n");
+		//print list1.list if not equal list2.list is false
 		out.print("list1.list == list2.list ? "+ (list1.list == list2.list));
-	}
+		}
 
-	
-}
+	}
 
